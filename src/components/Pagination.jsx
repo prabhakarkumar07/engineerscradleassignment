68996a07011ca456e31f3f10864e6fac1fed0b20
PrabhakarKumar <prabhakarkumarekc477@gmail.com>
2024-08-17 23:35:47 +0530

    imporved paggination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,23 +3,27 @@ import React from "react";
 function Pagination({ currentPage, totalPages, onPageChange }) {
   return (
     <div className="flex justify-between mt-4 gap-5">
-      <button
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
-      >
-        Previous
-      </button>
+      {/* Conditionally render Previous button */}
+      {currentPage > 1 && (
+        <button
+          onClick={() => onPageChange(currentPage - 1)}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+        >
+          Previous
+        </button>
+      )}
       <span>
         Page {currentPage} of {totalPages}
       </span>
-      <button
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
-      >
-        Next
-      </button>
+      {/* Conditionally render Next button */}
+      {currentPage < totalPages && (
+        <button
+          onClick={() => onPageChange(currentPage + 1)}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+        >
+          Next
+        </button>
+      )}
     </div>
   );
 }
